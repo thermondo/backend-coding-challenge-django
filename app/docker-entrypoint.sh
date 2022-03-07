b200cc90cb632a0aa56a11cbc1df6b186175e139
Jino Jossy <jinojossy93@gmail.com>
2022-03-08 02:08:16 +0530

    added entrypoint that can be used later

---
+++ app/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createcachetable
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_USERNAME
+fi
+
+$@
