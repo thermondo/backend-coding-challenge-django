c451fceeba06dc6fc1db1acbedb770f326947e78
Elias Rahmani <eliass.rahmani@gmail.com>
2022-05-03 18:46:37 +0430

    add user view

---
+++ app/user/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions, generics
+from user.serializers import UserSerializer, GroupSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
