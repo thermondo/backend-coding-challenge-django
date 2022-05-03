8721f620b560f97f94cf97d4ef6a9eb0358e8547
Elias Rahmani <eliass.rahmani@gmail.com>
2022-05-03 18:46:09 +0430

    add user application

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'note',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,12 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
